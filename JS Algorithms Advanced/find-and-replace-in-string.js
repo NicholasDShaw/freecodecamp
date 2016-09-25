6fb64768f1994a479168c65718815da1523d441a
Nicholas D Shaw <nick@nick-shaw.net>
2016-09-25 02:19:02 +0200

    updating find and replace in string algorithm

--- JS Algorithms Advanced/find-and-replace-in-string.js
+++ JS Algorithms Advanced/find-and-replace-in-string.js
@@ -1,8 +1,25 @@
-// find and replace in a string
+// find and replace in a string, preserving case
 function myReplace(str, before, after) {
+  var i=0;
+  var result=[];
+  while (i <= after.length){
+    charac = before.charAt(i);
+    if (!isNaN(charac * 1)){
+        result.push(after.charAt(i));
+    }else{
+      if (charac == charac.toUpperCase()) {
+        result.push(after.charAt(i).toUpperCase());
+      }
+      if (charac == charac.toLowerCase()){
+        result.push(after.charAt(i).toLowerCase());
+      }
+    }
+    i++;
+  }
+  after = result.join("");
   str = str.replace(before, after);
   return str;
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+myReplace("Let us get back to more Coding", "Coding", "algorithms");
 
