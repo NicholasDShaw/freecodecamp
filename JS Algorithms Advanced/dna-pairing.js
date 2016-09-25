3e9346dd2baf7daf2f2585e252d53a1850ca6c41
Nicholas D Shaw <nick@nick-shaw.net>
2016-09-25 13:20:04 +0200

    adding dna pairing

---
+++ JS Algorithms Advanced/dna-pairing.js
@@ -0,0 +1,26 @@
+//DNA Pairing
+
+function pairElement(str) {
+  
+  var pairs = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C"
+		};
+  var dna = [];
+  dna = str.split('').map(function(val){
+   var pair = [];
+   pair.push(val, pairs[val]);
+   return pair;
+  });
+  
+  console.log(str);
+  console.log(dna);
+  
+  
+  return str;
+  
+}
+
+pairElement("GCG");
