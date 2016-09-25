4dddef2d78b3d9bf90aa32f3f8ee7aaefe0ab655
Nicholas D Shaw <nick@nick-shaw.net>
2016-09-25 20:49:28 +0200

    Adding check-if-value-is-boolean.js

---
+++ JS Algorithms Advanced/check-if-value-is-boolean.js
@@ -0,0 +1,8 @@
+// check if value is boolean
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  bool = (bool === true || bool === false) ? true : false;
+  return bool;
+}
+
+booWho(null);
